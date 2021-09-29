bdab61fbe24c19a3093aa9cc0957b03d8ab758c5
ApenasGabs <Gabers357@gmail.com>
2021-09-29 00:09:37 -0300

    ja estou perto de conseguir, consigo sentir...

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const puppeteer = require("puppeteer");
+const produto = `Projetor+LG+CineBeam+Smart+TV+140`;
+const myPage = `https://www.google.com/search?q=${produto}`;
+console.log(myPage);
 
 async function robo() {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    const myPage = `https://www.kabum.com.br/produto/112807/projetor-lg-cinebeam-smart-tv-140-uhd-4k-hdr10-1500-ansi-lumens-hdmi-usb-bluetooth-wi-fi-branco-hu70la`;
+
     await page.goto(myPage);
     const resultado = await page.evaluate(() => {
-        return document.querySelector("div#cardAlertaOferta.sc-cVxdjG.kxXjpT");
+        return document.querySelector(
+            "div.pla-unit-container > div > div >div:nth-child(2)"
+        );
     });
     console.log(`ainda tem ${resultado} unidades`);
-    await browser.close();
+    // await browser.close();
 }
 robo();
--- index2.js
+++ index2.js
@@ -1,25 +1,25 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
-const url =
-    "https://www.kabum.com.br/produto/112807/projetor-lg-cinebeam-smart-tv-140-uhd-4k-hdr10-1500-ansi-lumens-hdmi-usb-bluetooth-wi-fi-branco-hu70la";
-// const url = "https://www.google.com/search?q=dolar";
+const request = require("request");
+const produto = `Projetor+LG+CineBeam+Smart+TV+140`;
+const url = `https://www.google.com/search?q=${produto}`;
+// const url = "https://www.kabum.com.br/produto/112807/projetor-lg-cinebeam-smart-tv-140-uhd-4k-hdr10-1500-ansi-lumens-hdmi-usb-bluetooth-wi-fi-branco-hu70la";
+
 console.log("1");
-axios(url)
-    .then((response) => {
-        const html = response.data;
+// axios(url)
+//     .then((response) => {
+//         const html = response.data;
+//         const $ = cheerio.load(html);
+//         const resto = $("top-pla-group-inner").next().text();
+//         console.log(resto);
+//     })
+//     .catch(console.error);
+request(`${url}`, (error, response, html) => {
+    if (!error && response.statusCode == 200) {
+
         const $ = cheerio.load(html);
-        const resto = $("sc-eQvJSV hZoTdn ").next().text();
-        console.log(resto);
-    })
-    .catch(console.error);
-// request(
-//     "https://www.google.com/search?q=dolar",
-//     (error, response, html) => {
-//         if (!error && response.statusCode == 200) {
-//             const $ = cheerio.load(html);
-//             const resto = $('.a61j6 vk_gy vk_sh Hg3mWc');
-//             console.log(resto);
-//             console.log("2");
-//         }
-//     }
-// );
+        const carrocel = $("div.pla-unit-container > div > div >div:nth-child(2)");
+        console.log(carrocel.html());
+        console.log("2");
+    }
+});
