57e77821cb650ece1d568af852050e048b7a55ee
Gabriel Rodrigues <52265978+Apenasgabs@users.noreply.github.com>
2021-09-29 22:42:03 -0300

    Feito primeiro codigo funcional 🤯

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const puppeteer = require("puppeteer");
-const produto = `Projetor+LG+CineBeam+Smart+TV+140`;
+const produto = `playstation 5`;
+// const produto = `Projetor LG CineBeam Smart TV 140`;
 const myPage = `https://www.google.com/search?q=${produto}`;
 console.log(myPage);
-
 async function robo() {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
-
     await page.goto(myPage);
     const resultado = await page.evaluate(() => {
         return document.querySelector(
             "div.pla-unit-container > div > div >div:nth-child(2)"
-        );
+        ).textContent;
     });
-    console.log(`ainda tem ${resultado} unidades`);
-    // await browser.close();
+    let edit = resultado.split(",");
+    console.log(`O ${produto} que voce quer`);
+    console.log(`Esta custando ${edit[0]}`);
+    await browser.close();
 }
 robo();
